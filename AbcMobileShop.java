2c875ac6a421de978c900228ae9204a67c4515be
Selvi@@123 <vkarvannan14@gmail.com>
2022-03-21 16:22:06 +0530

    Updated my code

---
+++ AbcMobileShop.java
@@ -0,0 +1,14 @@
+public class AbcMobileShop
+{
+	int discount;
+public AbcMobileShop()
+{
+System.out.println("jug Free");
+}
+public AbcMobileShop(int discount)
+{
+	this();
+	this.discount=discount;
+System.out.println("HO"+this.discount);
+}	
+}
